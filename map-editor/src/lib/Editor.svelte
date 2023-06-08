0044533da1778e87b32aa01b5bf980078437c1ae
Matt Pitts <matt.pitts@nuqleous.com>
2023-06-07 23:14:36 -0500

    load default maps, behavior

--- map-editor/src/lib/Editor.svelte
+++ map-editor/src/lib/Editor.svelte
@@ -1,12 +1,33 @@
 <script lang="ts">
-  let count: number = 0;
-  const increment = () => {
-    count += 1;
-  };
+  import type { CCSRWorld } from "src/map";
+  import { onMount } from "svelte";
+
+  const episode = 4;
+  let world: CCSRWorld;
+
+  async function loadWorld(episode: number): Promise<CCSRWorld> {
+    const request = await fetch(`maps/${episode}/map${episode}.json`);
+    return await request.json();
+  }
+
+  onMount(async () => {
+    world = await loadWorld(episode);
+  });
 </script>
 
-<div class="flex flex-row h-full">
-  <div class="basis-1/4 bg-slate-100 h-full">navigation</div>
-  <div class="basis-1/2 bg-slate-300">Map viewer</div>
-  <div class="basis-1/4 bg-slate-100">properties</div>
-</div>
+{#if world}
+  <div class="flex flex-row h-full">
+    <div class="basis-1/4 overflow-scroll">
+      navigation
+      {#each world.maps as map}
+        <div class="flex flex-col">
+          <button class="btn btn-square">
+            {map.name}
+          </button>
+        </div>
+      {/each}
+    </div>
+    <div class="basis-1/2">Map viewer</div>
+    <div class="basis-1/4">properties</div>
+  </div>
+{/if}
---
+++ map-editor/src/map.ts
@@ -0,0 +1,9 @@
+export interface CCSRWorld {
+  name: string;
+  maps: CCSRMap[];
+}
+
+export interface CCSRMap {
+  name: string;
+  data: string;
+}
