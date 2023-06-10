6d584df60149628e5c4f95d5f8b21f7364b44565
Matt P <5638426+mattbruv@users.noreply.github.com>
2023-06-10 12:35:45 -0500

    rename folder

--- map-editor/src/App.svelte
+++ map-editor/src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import Editor from "./lib/Editor.svelte";
-  import Nav from "./lib/Nav.svelte";
+  import Editor from "./components/Editor.svelte";
+  import Nav from "./components/Nav.svelte";
 </script>
 
 <div class="h-screen flex flex-col">
